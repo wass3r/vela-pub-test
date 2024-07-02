b5eca976dddd27f5729b17a004ff2b570d947f12
wass3r <1301201+wass3r@users.noreply.github.com>
2024-07-01 23:46:42 -0500

    more info

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,7 @@ echo -e "HTTP/1.1 200 OK\n\nHello, World!" | nc -l -p 900 &
 # Wait for the service to start
 sleep 1
 
+cat /etc/hosts
+
 # Try to reach the service using curl
 curl http://localhost:900
