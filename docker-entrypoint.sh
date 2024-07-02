01dbedff2f1df59d23baef931354172b177e233a
wass3r <1301201+wass3r@users.noreply.github.com>
2024-07-01 23:38:19 -0500

    lower port

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # simple service using nc
-echo -e "HTTP/1.1 200 OK\n\nHello, World!" | nc -l -p 8080 &
+echo -e "HTTP/1.1 200 OK\n\nHello, World!" | nc -l -p 900 &
 
 # Wait for the service to start
 sleep 1
 
 # Try to reach the service using curl
-curl http://localhost:8080
+curl http://localhost:900
